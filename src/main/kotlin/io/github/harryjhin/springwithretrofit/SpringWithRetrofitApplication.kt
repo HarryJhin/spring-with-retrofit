18d94089697d40416f7d76055231043e977eaa53
주진현 <joojinhyun00@gmail.com>
2024-10-10 16:36:08 +0900

    git init

---
+++ src/main/kotlin/io/github/harryjhin/springwithretrofit/SpringWithRetrofitApplication.kt
@@ -0,0 +1,11 @@
+package io.github.harryjhin.springwithretrofit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWithRetrofitApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWithRetrofitApplication>(*args)
+}
