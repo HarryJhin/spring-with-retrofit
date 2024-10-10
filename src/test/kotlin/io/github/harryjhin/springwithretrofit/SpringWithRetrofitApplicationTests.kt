a5a393f9b2a889caada5322cd62b51694a45c38a
주진현 <joojinhyun00@gmail.com>
2024-10-10 19:44:39 +0900

    통합 테스트 작성

--- src/test/kotlin/io/github/harryjhin/springwithretrofit/SpringWithRetrofitApplicationTests.kt
+++ src/test/kotlin/io/github/harryjhin/springwithretrofit/SpringWithRetrofitApplicationTests.kt
@@ -1,13 +1,34 @@
 package io.github.harryjhin.springwithretrofit
 
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
-class SpringWithRetrofitApplicationTests {
+@AutoConfigureMockMvc
+class SpringWithRetrofitApplicationTests(
+    @Autowired private val mockMvc: MockMvc,
+) {
+
+    @MockBean
+    private lateinit var mainService: MainService
 
     @Test
-    fun contextLoads() {
+    fun healthCheck() {
+        mockMvc.perform(get("/health"))
+            .andExpect(status().isOk)
     }
 
+    @Test
+    fun index() {
+        `when`(mainService.checkHealth()).thenReturn(true)
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk)
+    }
 }
