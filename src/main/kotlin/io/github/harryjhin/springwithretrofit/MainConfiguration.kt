59182a4a011be92e1895f44416b150882718f7e0
주진현 <joojinhyun00@gmail.com>
2024-10-10 21:09:23 +0900

    port 설정 최적화

--- src/main/kotlin/io/github/harryjhin/springwithretrofit/MainConfiguration.kt
+++ src/main/kotlin/io/github/harryjhin/springwithretrofit/MainConfiguration.kt
@@ -1,22 +1,22 @@
 package io.github.harryjhin.springwithretrofit
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 
 @Configuration
-class MainConfiguration(
-    private val objectMapper: ObjectMapper,
-) {
+class MainConfiguration {
 
     @Bean
-    fun mainClient(): MainClient {
-        return Retrofit.Builder()
-            .baseUrl("http://localhost:8080")
-            .addConverterFactory(JacksonConverterFactory.create(objectMapper))
-            .build()
-            .create(MainClient::class.java)
-    }
+    fun mainClient(
+        objectMapper: ObjectMapper,
+        @Value("\${server.port}") port: Int,
+    ): MainClient = Retrofit.Builder()
+        .baseUrl("http://localhost:$port")
+        .addConverterFactory(JacksonConverterFactory.create(objectMapper))
+        .build()
+        .create(MainClient::class.java)
 }
